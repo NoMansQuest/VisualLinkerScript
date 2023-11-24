2cf7454dc677410e6b12614ef7255d3f394104e5
Nasser GHOSEIRI <nasser.ghoseiri@contractors.roche.com>
2023-11-24 17:09:44 +0100

    Added wildcard support to the lexer

--- ParsingEngine/CLexer.cpp
+++ ParsingEngine/CLexer.cpp
@@ -132,6 +132,18 @@ namespace
                 (input == 'M'));
     }
 
+    /// @brief The following characters are considered wild-card characters. The list here is
+    ///        not exchaustive (*, ? and - can also be wildcards), however the other character's 
+    ///        role as wild-card as subjective. 
+    /// @param input 
+    /// @return 
+    bool IsWildCardCharacter(char input)
+    {
+        return (input == '[') ||
+               (input == ']') ||
+               (input == '\\');               
+    }
+
     /// @brief Detects if the input character is a white-space
     bool IsWhitespace(char input)
     {
@@ -389,6 +401,12 @@ std::shared_ptr<CRawFile> CLexer::ProcessLinkerScript(std::string absoluteFilePa
                 if (IsQuestionMark(currentCharacter))
                 {
                     parsedContent.emplace_back(CRawEntry(RawEntryType::QuestionMark, lineNumber, scanPosition, 1, parenthesisDepth, scopeDepth));
+                    break;
+                }
+
+                if (IsWildCardCharacter(currentCharacter))
+                {
+                    parsedContent.emplace_back(CRawEntry(RawEntryType::WildCardCharacter, lineNumber, scanPosition, 1, parenthesisDepth, scopeDepth));
                     break;
                 }
 
